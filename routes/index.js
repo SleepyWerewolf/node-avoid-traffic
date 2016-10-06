f583cb1283034364e1470bf09ccf9045fb180b23
Viet Truong <viettruong2194@gmail.com>
2016-10-05 21:33:01 -0700

    Add first pass of avoid-traffic lib using google maps api and test lib in index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const AvoidTraffic = require('../lib/google-client-avoid-traffic');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  const avoidTraffic = new AvoidTraffic(process.env.GOOGLE_API_KEY);
+
+  avoidTraffic.getData({
+    origins: ['7090 Mariposa St, San Diego'],
+    destinations: ['1135 City Lights Dr, Aliso Viejo'],
+    avoid: ['tolls'],
+    units: 'imperial',
+  })
+    .then(response => {
+      console.log(`Success: ${JSON.stringify(response)}`);
+      res.json(response);
+    })
+    .catch(error => {
+      console.log(`Error: ${JSON.stringify(error)}`);
+    });
 });
 
 module.exports = router;
